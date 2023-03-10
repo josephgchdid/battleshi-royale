5f5ca5361ee1933d9b60904fd492b6658fb02d73
josephgchdid <josefgchdid@gmail.com>
2023-03-10 21:17:29 +0200

    Added game repo interface

---
+++ game-service/game-service/Repository/Interface/IGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace game_service.Repository.Interface;
+
+public interface IGameRepository
+{
+    public Task CreateAsync<T>(T obj);
+
+    public Task<T> FindAsync<T>(Expression<Func<T, bool>> funcExpression, ProjectionDefinition<T> FieldsToExclude);
+
+    public Task<IEnumerable<T>> FindAllAsync<T>(Expression<Func<T, bool>> funcExpression, int limit, int skip);
+
+    public IMongoCollection<T> GetCollection<T>();
+}
