d1308fb4be80dd364105e6436877e969d9fd7218
josephgchdid <josefgchdid@gmail.com>
2023-03-13 12:43:39 +0200

    added boilerplate for game logic service

---
+++ logic-service/src/test/kotlin/com/example/logicservice/LogicServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.logicservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogicServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
