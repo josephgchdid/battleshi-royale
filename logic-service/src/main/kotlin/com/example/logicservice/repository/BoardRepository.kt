e4d7e26ff7259a8bc0270ddf645770e0788afa9d
josephgchdid <josefgchdid@gmail.com>
2023-03-15 12:50:33 +0200

    Changed repository Data type

--- logic-service/src/main/kotlin/com/example/logicservice/repository/BoardRepository.kt
+++ logic-service/src/main/kotlin/com/example/logicservice/repository/BoardRepository.kt
@@ -1,9 +1,10 @@
 package com.example.logicservice.repository
 
 import com.example.logicservice.entity.Board
+import com.example.logicservice.entity.CachedObject
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BoardRepository : CrudRepository<Board, String> {
+interface BoardRepository : CrudRepository<CachedObject, String> {
 }
