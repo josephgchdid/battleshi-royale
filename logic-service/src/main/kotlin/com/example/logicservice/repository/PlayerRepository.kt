3d7c9e83ae793250e1eb813aa208148288c09526
josephgchdid <josefgchdid@gmail.com>
2023-03-17 17:49:52 +0200

    added new dependency

--- logic-service/src/main/kotlin/com/example/logicservice/repository/PlayerRepository.kt
+++ logic-service/src/main/kotlin/com/example/logicservice/repository/PlayerRepository.kt
@@ -1,6 +1,5 @@
 package com.example.logicservice.repository
 
-import com.example.logicservice.entity.Board
 import com.example.logicservice.entity.CachedObject
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
