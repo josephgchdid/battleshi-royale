5cab1a3a0a03ed90d5e303c882bb3ffb920e386d
josephgchdid <josefgchdid@gmail.com>
2023-03-18 16:10:55 +0200

    moved classes to own folders

--- logic-service/src/main/kotlin/com/example/logicservice/entity/lobby/Lobby.kt
+++ logic-service/src/main/kotlin/com/example/logicservice/entity/lobby/Lobby.kt
@@ -2,10 +2,13 @@ package com.example.logicservice.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+
 
 @RedisHash("lobby")
 data class Lobby(
     @Id
+    @Indexed
     val gameId : String,
     val awaitingPlayers : Map<String, Boolean>
 )
